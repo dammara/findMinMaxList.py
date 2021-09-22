17decfce84aac72944894256e8b303a5e6a2af61
dammara <56162593+dammara@users.noreply.github.com>
2021-09-22 09:43:26 -0400

    Update findMinMaxList.py

--- findMinMaxList.py
+++ findMinMaxList.py
@@ -4,12 +4,11 @@
 
 user_list = []
 
-amt = int(input("How many numbers do you want in this list? >>>"))
+amt = int(input("How many numbers do you want in this list? >>>"))          # asks user how many values will be in list
 
-
-for amt in range(0, amt):
+for amt in range(0, amt):                                                   # appends list with each number inputted by user
     nums = input(f"Enter integer number {amt} >>>")
     user_list.append(nums)
 
-print(user_list)
-print("the minimum is: " + min(user_list) + "\nthe maximum is: " + max(user_list))
+print(user_list)                                                                        # prints list
+print("the minimum is: " + min(user_list) + "\nthe maximum is: " + max(user_list))      # prints min/max
